c6f7b47668dc83087c5bb14eb0b147654e4facf3
Dan <cornul11@gmail.com>
2021-03-14 17:34:36 +0100

    added shadows

--- Plamadeala_Raytracing_2/src/scene.cpp
+++ Plamadeala_Raytracing_2/src/scene.cpp
@@ -11,16 +11,13 @@
 
 using namespace std;
 
-pair<ObjectPtr, Hit> Scene::castRay(Ray const &ray) const
-{
+pair<ObjectPtr, Hit> Scene::castRay(Ray const &ray) const {
     // Find hit object and distance
     Hit min_hit(numeric_limits<double>::infinity(), Vector());
     ObjectPtr obj = nullptr;
-    for (unsigned idx = 0; idx != objects.size(); ++idx)
-    {
+    for (unsigned idx = 0; idx != objects.size(); ++idx) {
         Hit hit(objects[idx]->intersect(ray));
-        if (hit.t < min_hit.t)
-        {
+        if (hit.t < min_hit.t) {
             min_hit = hit;
             obj = objects[idx];
         }
@@ -29,8 +26,7 @@ pair<ObjectPtr, Hit> Scene::castRay(Ray const &ray) const
     return pair<ObjectPtr, Hit>(obj, min_hit);
 }
 
-Color Scene::trace(Ray const &ray, unsigned depth)
-{
+Color Scene::trace(Ray const &ray, unsigned depth) {
     pair<ObjectPtr, Hit> mainhit = castRay(ray);
     ObjectPtr obj = mainhit.first;
     Hit min_hit = mainhit.second;
@@ -60,9 +56,17 @@ Color Scene::trace(Ray const &ray, unsigned depth)
     Color color = material.ka * matColor;
 
     // Add diffuse and specular components.
-    for (auto const &light : lights)
-    {
+    for (auto const &light : lights) {
         Vector L = (light->position - hit).normalized();
+
+        //Render shadows
+        if (renderShadows) {
+            Ray shadow(hit + (epsilon * shadingN), L);
+            pair<ObjectPtr, Hit> s_hit = castRay(shadow);
+            if (s_hit.second.t < (light->position - hit).length()) {
+                continue;
+            }
+        }
 
         // Add diffuse.
         double dotNormal = shadingN.dot(L);
@@ -70,8 +74,7 @@ Color Scene::trace(Ray const &ray, unsigned depth)
         color += diffuse * material.kd * light->color * matColor;
 
         // Add specular.
-        if(dotNormal > 0)
-        {
+        if (dotNormal > 0) {
             Vector reflectDir = reflect(-L, shadingN); // Note: reflect(..) is not given in the framework.
             double specAngle = std::max(reflectDir.dot(V), 0.0);
             double specular = std::pow(specAngle, material.n);
@@ -80,27 +83,67 @@ Color Scene::trace(Ray const &ray, unsigned depth)
         }
     }
 
-    if (depth > 0 and material.isTransparent)
-    {
+    if (depth > 0 and material.isTransparent) {
         // The object is transparent, and thus refracts and reflects light.
-        // Use Schlick's approximation to determine the ratio between the two.
-    }
-    else if (depth > 0 and material.ks > 0.0)
-    {
+        Vector reflectDir = reflect(ray.D, shadingN);
+        Ray reflectRay(hit + (epsilon * shadingN), reflectDir);
+
+        double DdotN = ray.D.dot(N);
+        double ni = 1.0;
+        double nt = material.nt;
+        int outside = 0;
+
+        if (DdotN < 0) {
+            DdotN = -DdotN;
+            outside = 1;
+        } else {
+            shadingN = -shadingN;
+            N = -N;
+            std::swap(nt, ni);
+        }
+
+        double refRatio = ni / nt;
+        double k = 1 - refRatio * refRatio * (1 - DdotN * DdotN);
+
+        Vector refractDir;
+        if (k < 0) {
+            refractDir = Vector(0);
+        } else {
+            refractDir = refRatio * ray.D + (refRatio * DdotN - sqrt(k)) * N;
+        }
+        refractDir.normalize();
+
+        Vector refractRayFrom;
+        if (outside == 1) {
+            refractRayFrom = hit - (epsilon * shadingN);
+        } else {
+            refractRayFrom = hit + (epsilon * shadingN);
+        }
+
+        //Schlick's approximation to determine the ratio between the two.
+        double kr0 = ((ni - nt) / (ni + nt)) * ((ni - nt) / (ni + nt));
+        double kr = kr0 + (1.0 - kr0) * pow(1.0 - DdotN, 5);
+
+        Ray refractRay(refractRayFrom, refractDir);
+
+        color += trace(reflectRay, depth - 1) * kr +
+            trace(refractRay, depth - 1) * (1.0 - kr);
+    } else if (depth > 0 and material.ks > 0.0) {
         // The object is not transparent, but opaque.
+        Vector reflectDir = reflect(ray.D, shadingN);
+        Ray reflectRay(hit + (epsilon * shadingN), reflectDir);
+        color += material.ks * trace(reflectRay, depth - 1);
     }
 
     return color;
 }
 
-void Scene::render(Image &img)
-{
+void Scene::render(Image &img) {
     unsigned w = img.width();
     unsigned h = img.height();
 
     for (unsigned y = 0; y < h; ++y)
-        for (unsigned x = 0; x < w; ++x)
-        {
+        for (unsigned x = 0; x < w; ++x) {
             Point pixel(x + 0.5, h - 1 - y + 0.5, 0);
             Ray ray(eye, (pixel - eye).normalized());
             Color col = trace(ray, recursionDepth);
@@ -113,51 +156,42 @@ void Scene::render(Image &img)
 
 // Defaults
 Scene::Scene()
-:
+    :
     objects(),
     lights(),
     eye(),
     renderShadows(false),
     recursionDepth(0),
-    supersamplingFactor(1)
-{}
+    supersamplingFactor(1) {}
 
-void Scene::addObject(ObjectPtr obj)
-{
+void Scene::addObject(ObjectPtr obj) {
     objects.push_back(obj);
 }
 
-void Scene::addLight(Light const &light)
-{
+void Scene::addLight(Light const &light) {
     lights.push_back(LightPtr(new Light(light)));
 }
 
-void Scene::setEye(Triple const &position)
-{
+void Scene::setEye(Triple const &position) {
     eye = position;
 }
 
-unsigned Scene::getNumObject()
-{
+unsigned Scene::getNumObject() {
     return objects.size();
 }
 
-unsigned Scene::getNumLights()
-{
+unsigned Scene::getNumLights() {
     return lights.size();
 }
 
-void Scene::setRenderShadows(bool shadows)
-{
+void Scene::setRenderShadows(bool shadows) {
     renderShadows = shadows;
 }
 
-void Scene::setRecursionDepth(unsigned depth)
-{
+void Scene::setRecursionDepth(unsigned depth) {
     recursionDepth = depth;
 }
 
-void Scene::setSuperSample(unsigned factor)
-{
+void Scene::setSuperSample(unsigned factor) {
     supersamplingFactor = factor;
 }
